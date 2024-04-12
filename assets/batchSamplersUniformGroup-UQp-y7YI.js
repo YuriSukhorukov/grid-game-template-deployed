173a137cef6d6a641e469bac47a18df01a498f93
Yuri Sukhorukov <u.suhorukov@3nt.dev>
2024-04-12 18:09:14 +0400

    git push

---
+++ assets/batchSamplersUniformGroup-UQp-y7YI.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-C3_3mXaP.js";import{U as o}from"./colorToUniform-CVm2J-KT.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
